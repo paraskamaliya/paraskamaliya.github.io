1e581c0b6371a2d5260c27a0ac8b4da765b7994c
Paras <paraskamliya5@gmail.com>
2023-09-16 18:23:11 +0530

    done

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -9,10 +9,10 @@ const Navbar = () => {
         <nav id="nav-menu">
             <img src={logo} alt="Logo" className="logo" />
             <div className="desktopMenu">
-                <Link id="home" className="desktopMenuList">Home</Link>
-                <Link id="about" className="about section desktopMenuList">About</Link>
-                <Link id="skills" className="desktopMenuList">Skills</Link>
-                <Link id="projects" className="desktopMenuList">Projects</Link>
+                <Link id="home" className="desktopMenuList" to="/">Home</Link>
+                <Link id="about" className="about section desktopMenuList" to="/about">About</Link>
+                <Link id="skills" className="desktopMenuList" to="/skills">Skills</Link>
+                <Link id="projects" className="desktopMenuList" to="/projects">Projects</Link>
             </div>
             <button className="desktopMenuBtn" id="contact">
                 <img src={contactimg} alt="" className="desktopMenuImg" />Contact Me</button>
