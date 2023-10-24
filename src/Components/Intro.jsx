d2f9f0dd3791350f23f342d702cd1b9d71a1fe73
Paras <paraskamliya5@gmail.com>
2023-10-24 09:46:07 +0530

    done

--- src/Components/Intro.jsx
+++ src/Components/Intro.jsx
@@ -1,7 +1,7 @@
 import { Box, Button, Heading, Image, Stack, Text } from "@chakra-ui/react"
 import ProfilePic from "../Photos/profilepic.png"
 import { useSelector } from "react-redux";
-import { ExternalLinkIcon } from "@chakra-ui/icons";
+import { DownloadIcon } from "@chakra-ui/icons";
 import resume from "../assets/ParasKamaliya_Resume.pdf";
 const Intro = () => {
     const theme = useSelector(store => store.theme);
@@ -20,7 +20,7 @@ const Intro = () => {
                     <Heading textAlign={"left"} fontSize={"3.5rem"} >Full Stack Web Developer</Heading>
                     <Text textAlign={"left"} fontSize={"1.2rem"} noOfLines={2} fontWeight={300} >Skilled Full stack Web developer and experienced <br /> in creating User freindly Interface websites.</Text>
                     <a href={resume} id="resume-link-2" target="_blank" rel="noreferrer" >
-                        <Button rightIcon={<ExternalLinkIcon />} onClick={downloadAndOpenResume} bg={theme === "dark" ? "white" : "black"} color={theme === "dark" ? "black" : "white"} _hover={{ color: "none" }} id="resume-button-2" >Resume</Button>
+                        <Button rightIcon={<DownloadIcon />} onClick={downloadAndOpenResume} bg={theme === "dark" ? "white" : "black"} color={theme === "dark" ? "black" : "white"} _hover={{ color: "none" }} id="resume-button-2" >Resume</Button>
                     </a>
                 </Box>
                 <Box w={["100%", "85%", "70%", "50%", "50%"]} m={"auto"} position={"sticky"} h={"100%"} alignItems={"flex-end"}>
