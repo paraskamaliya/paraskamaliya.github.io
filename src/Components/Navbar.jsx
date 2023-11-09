dd8747cf78ee1319a70e5a0242edd4c5faa8098e
Paras <paraskamliya5@gmail.com>
2023-11-09 10:23:44 +0530

    done

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -24,7 +24,6 @@ const Navbar = () => {
             },
             body: JSON.stringify(payload)
         })
-            .then((res) => res.json())
             .then((res) => console.log(res))
             .catch((err) => console.log(err))
     }
